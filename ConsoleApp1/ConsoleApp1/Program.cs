bbfe8d310a5fa77becb2b1b4a927923dfba48960
Murad Aslanov <tu71tfsws@aztu.local>
2022-04-20 19:07:26 +0400

    first commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -14,9 +14,13 @@ namespace ConsoleApp1
             int personcapacity = Convert.ToInt32(Console.ReadLine());
             int personcapacity1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter isvailable");
-            bool isvailable = Convert.ToInt32(Console.ReadLine());
-            Room room = new Room(price, personcapacity, true,"m");
-            Room room1 = new Room(price1, personcapacity, true,"m");
+            bool isvailable = Convert.ToBoolean(Console.ReadLine());
+            bool isvailable1 = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("enter name");
+            string name = Console.ReadLine();
+            string name1 = Console.ReadLine();
+            Room room = new Room(price, personcapacity, isvailable, name);
+            Room room1 = new Room(price1, personcapacity1, isvailable1, name1);
             Hotel hotel = new Hotel("b");
 
             hotel.AddRoom(room);
